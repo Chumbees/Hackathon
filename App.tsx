10c6edc477289258216ba186d6561b8cabe2263a
Nick Chambers <nicholas.chambers@qvc.com>
2019-09-03 15:31:09 -0400

    Including some example components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      {/* Example of colored view and text */}
+      <View style={{backgroundColor: 'blue'}}>
+        <Text style={{color: '#fff'}}>White text blue box</Text>
+      </View>
+
+      {/* Uncolored text */}
       <Text>If you can read this you did it right. -Chumbees</Text>
+
+      {/* Example button */}
+      <Button
+        title='example button'
+        // onPress={}
+        />
+
     </View>
   );
 }
