a928a98e3767df31783d971e0f6cc2772fb6d9ce
Nick Chambers <nicholas.chambers@qvc.com>
2019-09-03 13:07:51 -0400

    First commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>If you can read this you did it right. -Chumbees</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
